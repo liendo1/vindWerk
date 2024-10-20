a637213f9708ad27dcd18629ebdffa7e08320d12
michael liendo <44812892+liendo1@users.noreply.github.com>
2024-10-20 23:05:33 +0200

    now getting the info from database

--- frontend/src/component/home/home.component.ts
+++ frontend/src/component/home/home.component.ts
@@ -1,17 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {JobListingCardComponent} from '../job-listing-card/job-listing-card.component';
 import {JobDetailCardComponent} from '../job-detail-card/job-detail-card.component';
+import {JobService} from '../../service/job.service';
+import {JobDto} from '../../dto/job-dto';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     JobListingCardComponent,
-    JobDetailCardComponent
+    JobDetailCardComponent,
+    NgForOf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  jobsList:JobDto[] = [];
+
+  constructor(private jobService: JobService) {
+  }
+
+  ngOnInit() {
+    this.jobService.getAllJobs().subscribe(
+      (jobs: JobDto[]) => {
+        this.jobsList = jobs;
+      },
+      (error) => {
+        console.error("Error fetching jobs:", error);
+      }
+    );
+  }
+
+
+
+
+
+
 
 }
+function ngOnInit() {
+    throw new Error('Function not implemented.');
+}
+
