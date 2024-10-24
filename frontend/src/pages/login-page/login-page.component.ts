8324e8156631a970356e713b59ee663a2be10962
michael liendo <44812892+liendo1@users.noreply.github.com>
2024-10-24 10:55:37 +0200

    made the login_page

--- frontend/src/pages/login-page/login-page.component.ts
+++ frontend/src/pages/login-page/login-page.component.ts
@@ -3,15 +3,15 @@ import {LoginComponent} from '../../component/login/login.component';
 import {RegisterComponent} from '../../component/register/register.component';
 
 @Component({
-  selector: 'app-login-register-page',
+  selector: 'app-login-page',
   standalone: true,
   imports: [
     LoginComponent,
     RegisterComponent
   ],
-  templateUrl: './login-register-page.component.html',
-  styleUrl: './login-register-page.component.css'
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
 })
-export class LoginRegisterPageComponent {
+export class LoginPageComponent {
 
 }
